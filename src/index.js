4b32f0066f3a56818fa9ad779ac0e85adf8c3ee0
sofi <labyak ya HUSSAIN@sufi>
2020-05-18 16:24:28 +0530

    add

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
-app.use(userRouter);
-
+app.use(userRouter)
 app.listen(port , () =>{
     console.log('app is listening on port '+port)
 });
